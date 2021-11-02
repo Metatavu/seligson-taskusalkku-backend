c0260017956589d2cfaa2d7e98c6baa5d76efeb3
Antti Leppä <antti.leppa@metatavu.fi>
2021-11-02 07:52:31 +0200

    Added an initial version of openapi generator

---
+++ bin/utils/generate-spec.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+PROJECT_DIR=$(realpath $SCRIPT_DIR/../../)
+rm -fR $PROJECT_DIR/generated
+mkdir -p generated
+$SCRIPT_DIR/openapi-generator-cli.sh generate -i taskusalkku-spec/swagger.yaml -g python-fastapi -o $PROJECT_DIR/generated --config $PROJECT_DIR/generator-config.json -t openapi-templates
+rm -fR src/spec
+rm -f $PROJECT_DIR/generated/src/spec/main.py
+cp -R $PROJECT_DIR/generated/src/spec src/spec
+rm -fR $PROJECT_DIR/generated
