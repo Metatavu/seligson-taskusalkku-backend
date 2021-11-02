fe75ed84374fe87e4349b632e319db1a2c025863
Antti Leppä <antti.leppa@metatavu.fi>
2021-11-02 08:01:57 +0200

    Changed system_api to use generated spec

---
+++ src/impl/apis/system_api.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+from fastapi_utils.cbv import cbv
+from ...spec.apis.system_api import SystemApiSpec, router as SystemApiRouter
+
+
+@cbv(SystemApiRouter)
+class SystemApiImpl(SystemApiSpec):
+
+    async def ping(self) -> str:
+        return "pong"
