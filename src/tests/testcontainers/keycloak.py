7ed82d64fc8d09b47365d2f61fe113230ae34132
Antti Leppä <antti.leppa@metatavu.fi>
2021-11-04 07:57:32 +0200

    Added a test container for Keycloak

---
+++ src/tests/testcontainers/keycloak.py
@@ -0,0 +1,63 @@
+from testcontainers.core.generic import DockerContainer
+from testcontainers.core.waiting_utils import wait_for_logs
+from os import environ
+
+
+class KeycloakContainer(DockerContainer):
+    """
+    Keycloak container.
+    -------
+    """
+    KEYCLOAK_USER = environ.get("KEYCLOAK_USER", "admin")
+    KEYCLOAK_PASSWORD = environ.get("KEYCLOAK_PASSWORD", "admin")
+    KEYCLOAK_IMPORT = environ.get("KEYCLOAK_IMPORT")
+    
+    def __init__(self, image="quay.io/keycloak/keycloak:latest", **kwargs):
+        """Constructor
+
+        Args:
+            image (str, optional): Used Docker image. Defaults to "quay.io/keycloak/keycloak:latest".
+            KEYCLOAK_USER (str, optional): Keycloak admin user. Defaults to admin
+            KEYCLOAK_PASSWORD (str, optional): Keycloak admin password. Defaults to admin
+            KEYCLOAK_IMPORT (str, optional): Path to Keycloak realm import file.
+        """        
+        super(KeycloakContainer, self).__init__(image)
+        self.port_to_expose = 8080
+        self.with_exposed_ports(self.port_to_expose)
+        if 'KEYCLOAK_USER' in kwargs:
+            self.KEYCLOAK_USER = kwargs['KEYCLOAK_USER']
+        if 'KEYCLOAK_PASSWORD' in kwargs:
+            self.KEYCLOAK_PASSWORD = kwargs['KEYCLOAK_PASSWORD']
+        if 'KEYCLOAK_IMPORT' in kwargs:
+            self.KEYCLOAK_IMPORT = kwargs['KEYCLOAK_IMPORT']
+
+    def start(self, timeout=60):
+        """Starts the Keycloak and waits for it to be ready.
+
+        Args:
+            timeout (int, optional): Timeout for container to be ready. Defaults to 60.
+        """        
+        self._configure()
+        super().start()
+        wait_for_logs(self, r'Resuming server\n', timeout=timeout)
+        return self
+
+    def get_keycloak_url(self):
+        """Returns Keycloak's URL
+
+        Returns:
+            str: Keycloak's URL
+        """        
+        host = self.get_container_host_ip()
+        port = self.get_exposed_port(8080)
+        return "http://" + host + ":" + port
+
+    def _configure(self):
+        """Configures the Keycloak container
+        """        
+        self.with_env("KEYCLOAK_USER", self.KEYCLOAK_USER)
+        self.with_env("KEYCLOAK_PASSWORD", self.KEYCLOAK_PASSWORD)
+
+        if self.KEYCLOAK_IMPORT:
+            self.with_volume_mapping(self.KEYCLOAK_IMPORT, "/tmp/kc.json")
+            self.with_env("KEYCLOAK_IMPORT", "/tmp/kc.json")
