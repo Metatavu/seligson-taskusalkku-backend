d2a9efd874f0c52e11e74ebfaf3121373979c3ec
Antti Leppä <antti.leppa@metatavu.fi>
2021-11-11 08:41:58 +0200

    Sonar issues

--- src/tests/fixtures/mysql.py
+++ src/tests/fixtures/mysql.py
@@ -5,8 +5,10 @@ import logging
 from testcontainers.mysql import MySqlContainer
 
 logger = logging.getLogger(__name__)
-sql_import_file = os.path.join(os.path.dirname(__file__), '..', 'data', 'db.sql')
-container_file="/tmp/import.sql"
+data_dir = os.path.join(os.path.dirname(__file__), '..', 'data')
+sql_import_file = os.path.join(data_dir, 'db.sql')
+container_file = "/tmp/import.sql"  # NOSONAR
+
 
 @pytest.fixture(scope="session")
 def mysql(request):
@@ -17,19 +19,19 @@ def mysql(request):
 
     Returns:
         MySqlContainer: Reference to MySQL container
-    """    
+    """
 
     mysql = MySqlContainer('mariadb:10.3.29')
     mysql.with_volume_mapping(sql_import_file, container_file)
     mysql.start()
-    
+
     import_command = f'bash -c "mysql -uroot -ptest test < {container_file}"'
     import_result = mysql.exec(import_command)
     assert import_result.exit_code == 0
 
     def teardown():
         """Stops the containers after session
-        """        
+        """
         mysql.stop()
 
     request.addfinalizer(teardown)
