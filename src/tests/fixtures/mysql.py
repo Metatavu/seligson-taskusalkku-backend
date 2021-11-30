4cb861aaa12f84bc54be97d81204bda633ecf0de
Antti Leppä <antti.leppa@metatavu.fi>
2021-11-30 14:53:31 +0200

    Updated mysql container to use bin log

--- src/tests/fixtures/mysql.py
+++ src/tests/fixtures/mysql.py
@@ -6,8 +6,14 @@ from testcontainers.mysql import MySqlContainer
 
 logger = logging.getLogger(__name__)
 data_dir = os.path.join(os.path.dirname(__file__), '..', 'data')
-sql_import_files = ['db.sql', 'raterah.sql']
 container_import_folder = "/tmp/import"  # NOSONAR
+
+
+def mysql_import_sql(mysql: MySqlContainer, sql_import_file: str):
+    logger.info(f"Importing SQL file {sql_import_file}...")
+    import_command = f'bash -c "mysql -uroot -ptest test < {container_import_folder}/{sql_import_file}"'
+    import_result = mysql.exec(import_command)
+    assert import_result.exit_code == 0
 
 
 @pytest.fixture(scope="session")
@@ -23,13 +29,13 @@ def mysql(request):
 
     mysql = MySqlContainer('mariadb:10.3.29')
     mysql.with_volume_mapping(data_dir, container_import_folder)
+    mysql.with_command("--server-id=1 --log-bin=/var/log/mysql/mysql-bin.log --binlog_format=ROW --binlog_do_db=test")
     mysql.start()
     os.environ["SQLALCHEMY_DATABASE_URL"] = mysql.get_connection_url()
 
-    for sql_import_file in sql_import_files:
-        import_command = f'bash -c "mysql -uroot -ptest test < {container_import_folder}/{sql_import_file}"'
-        import_result = mysql.exec(import_command)
-        assert import_result.exit_code == 0
+    mysql_import_sql(mysql, 'db.sql')
+    mysql_import_sql(mysql, 'fund-securities.sql')
+    mysql_import_sql(mysql, 'raterah.sql')
 
     def teardown():
         """Stops the containers after session
