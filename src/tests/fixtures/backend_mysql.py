f85389d60baa43263ac93cb1dc7e2fe1f164f517
Antti Leppä <antti.leppa@metatavu.fi>
2021-11-30 17:13:06 +0200

    Added alembic

--- src/tests/fixtures/backend_mysql.py
+++ src/tests/fixtures/backend_mysql.py
@@ -2,10 +2,12 @@ import pytest
 import os
 import logging
 
+from alembic.config import Config
+from alembic import command
 from testcontainers.mysql import MySqlContainer
 
 logger = logging.getLogger(__name__)
-
+alembicini = os.path.join(os.path.dirname(__file__), '..', '..', '..', 'alembic.ini')
 
 @pytest.fixture(scope="session")
 def backend_mysql(request):
@@ -19,8 +21,11 @@ def backend_mysql(request):
     """
 
     mysql = MySqlContainer('mysql:5.6')
+    mysql.with_command("--character-set-server=utf8mb4 --collation-server=utf8mb4_unicode_ci")
     mysql.start()
     os.environ["DATABASE_URL"] = mysql.get_connection_url()
+    alembic_cfg = Config(alembicini)
+    command.upgrade(alembic_cfg, 'head')
 
     def teardown():
         """Stops the containers after session
