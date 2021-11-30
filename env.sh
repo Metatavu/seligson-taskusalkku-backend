ed291d0261fd5caf326510e892f872f3d29a54e3
Tianxing Wu <tianxing.wu@metatavu.fi>
2021-11-30 13:54:03 +0200

    env.sh

--- env.sh
+++
@@ -1,7 +0,0 @@
-export SQLALCHEMY_DATABASE_URL=mysql://root:root@localhost:5432/seli
-export OIDC_AUTH_SERVER_URL=127.0.0.1:8080/auth
-export OIDC_AUDIENCE=seli-client
-export FUND_JSON=tests/data/funds.json
-export FUND_OPTIONS_JSON=tests/data/fund-options.json
-export FUND_VALUES_BASIC_CSV=tests/data/fund-values-basic.csv
-export HOLIDAYS_CSV=tests/data/holidays.csv
