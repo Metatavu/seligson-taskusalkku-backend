d0b56a501718a69c2b8a3e634a3895e40bb3ae30
baherami <baherami@gmail.com>
2021-12-09 14:03:40 +0200

    fix sql files

--- src/tests/data/backend-company.sql
+++ src/tests/data/backend-company.sql
@@ -1,4 +1,4 @@
-INSERT INTO company (id, company_code, ssn) VALUES ((UNHEX(REPLACE(UUID(), "-",""))), '123','010170-999R');
-INSERT INTO company (id, company_code, ssn) VALUES ((UNHEX(REPLACE(UUID(), "-",""))), '124','010170-999R');
-INSERT INTO company (id, company_code, ssn) VALUES ((UNHEX(REPLACE(UUID(), "-",""))), '125','010170-999R');
-INSERT INTO company (id, company_code, ssn) VALUES ((UNHEX(REPLACE(UUID(), "-",""))), '126','12345567');
+INSERT INTO company (id, original_id, ssn) VALUES ((UNHEX(REPLACE(UUID(), "-",""))), '123','010170-999R');
+INSERT INTO company (id, original_id, ssn) VALUES ((UNHEX(REPLACE(UUID(), "-",""))), '124','010170-999R');
+INSERT INTO company (id, original_id, ssn) VALUES ((UNHEX(REPLACE(UUID(), "-",""))), '125','010170-999R');
+INSERT INTO company (id, original_id, ssn) VALUES ((UNHEX(REPLACE(UUID(), "-",""))), '126','12345567');
