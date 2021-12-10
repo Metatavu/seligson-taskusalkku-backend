487897ae456848e2fb27ed55ea6c49c4fe18233b
Antti Leppä <antti.leppa@metatavu.fi>
2021-12-10 12:41:46 +0200

    Added failsafe sleep to mssql container start

--- src/tests/testcontainers/mssql.py
+++ src/tests/testcontainers/mssql.py
@@ -1,3 +1,5 @@
+import time
+
 from testcontainers.core.waiting_utils import wait_for_logs
 from testcontainers.mssql import SqlServerContainer as SqlServerContainerBase
 
@@ -6,6 +8,7 @@ class SqlServerContainer(SqlServerContainerBase):
 
     def _connect(self):
         wait_for_logs(self, r'SQL Server is now ready for client connections', timeout=60)
+        time.sleep(10)
 
     def _configure(self):
         self.with_env("MSSQL_AGENT_ENABLED", "true")
