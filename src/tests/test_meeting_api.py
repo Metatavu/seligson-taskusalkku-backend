64357c224a992ea8bc19b4572b14c561c7075db6
Antti Leppä <antti.leppa@metatavu.fi>
2021-12-21 10:54:14 +0200

    Review changes

--- src/tests/test_meeting_api.py
+++ src/tests/test_meeting_api.py
@@ -12,28 +12,29 @@ from .fixtures.client import *  # noqa
 
 import json
 
-example_meeting = {
-    "type": "MEETING",
-    "time": "2021-12-01T13:11:24.565341",
-    "language": "fi",
-    "participantCount": 1,
-    "additionalInformation": "additional info",
-    "contact": {
-        "firstName": "tommi",
-        "lastName": "tommi",
-        "email": "tommi@example.fi",
-        "phone": "00000",
-    }
-}
-
 
 class TestMeeting:
     """Tests for meeting endpoints"""
 
+    def setup_method(self):
+        self.example_meeting = {
+            "type": "MEETING",
+            "time": "2021-12-01T13:11:24.565341",
+            "language": "fi",
+            "participantCount": 1,
+            "additionalInformation": "additional info",
+            "contact": {
+                "firstName": "tommi",
+                "lastName": "tommi",
+                "email": "tommi@example.fi",
+                "phone": "00000",
+            }
+        }
+
     def test_create_meeting(self, client: TestClient, user_1_auth: BearerAuth, smtp: SmtpContainer):
-        response = client.post(f"/v1/meetings", auth=user_1_auth, data=json.dumps(example_meeting))
+        response = client.post(f"/v1/meetings", auth=user_1_auth, data=json.dumps(self.example_meeting))
         assert response.status_code == 200
-        assert example_meeting == response.json()
+        assert self.example_meeting == response.json()
 
     @pytest.mark.parametrize("auth", invalid_auths)
     def test_create_meeting_invalid_auth(self, client: TestClient, backend_mysql: MySqlContainer,
@@ -42,14 +43,14 @@ class TestMeeting:
             client=client,
             expected_status=403,
             auth=auth,
-            payload=example_meeting
+            payload=self.example_meeting
         )
 
     def test_create_meeting_anonymous(self, client: TestClient, backend_mysql: MySqlContainer,
                                       keycloak: KeycloakContainer, anonymous_auth: BearerAuth):
-        response = client.post(f"/v1/meetings", auth=anonymous_auth, data=json.dumps(example_meeting))
+        response = client.post(f"/v1/meetings", auth=anonymous_auth, data=json.dumps(self.example_meeting))
         assert response.status_code == 200
-        assert example_meeting == response.json()
+        assert self.example_meeting == response.json()
 
     def test_create_meeting_unauthorized(self, client: TestClient, backend_mysql: MySqlContainer,
                                          keycloak: KeycloakContainer):
@@ -57,7 +58,7 @@ class TestMeeting:
             client=client,
             expected_status=403,
             auth=None,
-            payload=example_meeting
+            payload=self.example_meeting
         )
 
     def test_find_meeting_times(self, client: TestClient, user_1_auth: BearerAuth):
