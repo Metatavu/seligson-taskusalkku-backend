c7999ca141708e512d76d0cf5f4252a6e93681de
Antti Leppä <antti.leppa@metatavu.fi>
2022-02-14 14:39:10 +0200

    Added fund group column to fund table

---
+++ src/alembic/versions/0016_add_fund_group.py
@@ -0,0 +1,29 @@
+"""add fund group column to fund table
+
+Revision ID: 0008
+Revises: 0007
+Create Date: 2021-12-17 11:00:00.000000
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '0016'
+down_revision = '0015'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column("fund", sa.Column("fund_group", sa.String(192), nullable=True))
+    op.execute("UPDATE fund SET fund_group = 'ACTIVE' WHERE original_id in ('3333','88476','88537','8888')")
+    op.execute("UPDATE fund SET fund_group = 'BALANCED' WHERE original_id in ('818419')")
+    op.execute("UPDATE fund SET fund_group = 'FIXED_INCOME' WHERE original_id in ('1111','2222','580')")
+    op.execute("UPDATE fund SET fund_group = 'DIMENSION' WHERE original_id in ('DIMESCB','DIMUSCB','DIMEMVB')")
+    op.execute("UPDATE fund SET fund_group = 'SPILTAN' WHERE original_id in ('SPILTAN SVERIGE','SPILTAN STABIL')")
+    op.execute("UPDATE fund SET fund_group = 'PASSIVE' WHERE original_id in ('800','5555','818423','7777','795',"
+               "'88511','581','4444')")
+
+def downgrade():
+    op.drop_column("fund", column_name="fund_group")
