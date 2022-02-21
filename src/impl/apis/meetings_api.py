be0f0c1ace730ad29302d76af08a72ab4930d449
baherami <baherami@gmail.com>
2022-02-21 16:35:07 +0200

    Fix variables

--- src/impl/apis/meetings_api.py
+++ src/impl/apis/meetings_api.py
@@ -12,11 +12,6 @@ from fastapi import HTTPException
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
 from business_logics import business_logics
 
-MAIL_USERNAME = os.environ["MAIL_USERNAME"]
-MAIL_PASSWORD = os.environ["MAIL_PASSWORD"]
-USE_CREDENTIALS = bool(MAIL_USERNAME) and bool(MAIL_PASSWORD)
-VALIDATE_CERTS = USE_CREDENTIALS
-
 
 @cbv(meetings_api_router)
 class MeetingsApiImpl(MeetingsApiSpec):
@@ -27,16 +22,21 @@ class MeetingsApiImpl(MeetingsApiSpec):
         token_bearer: TokenModel
     ) -> Meeting:
 
+        mail_username = os.environ["MAIL_USERNAME"]
+        mail_password = os.environ["MAIL_PASSWORD"]
+        use_credentials = bool(mail_username) and bool(mail_password)
+        validate_certs = use_credentials
+
         email_conf = ConnectionConfig(
-            MAIL_USERNAME=MAIL_USERNAME,
-            MAIL_PASSWORD=MAIL_PASSWORD,
+            MAIL_USERNAME=mail_username,
+            MAIL_PASSWORD=mail_password,
             MAIL_FROM=os.environ["MAIL_FROM"],
             MAIL_PORT=os.environ["MAIL_PORT"],
             MAIL_SERVER=os.environ["MAIL_SERVER"],
             MAIL_TLS=eval(os.environ["MAIL_TLS"]),
             MAIL_SSL=eval(os.environ["MAIL_SSL"]),
-            USE_CREDENTIALS=USE_CREDENTIALS,
-            VALIDATE_CERTS=VALIDATE_CERTS
+            USE_CREDENTIALS=use_credentials,
+            VALIDATE_CERTS=validate_certs
         )
 
         email_body = ""
