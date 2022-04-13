ce0460a383d6eae2c2e130716a8559677150a32c
Antti Leppä <antti.leppa@metatavu.fi>
2022-04-13 18:33:53 +0300

    Added test data for company access features

--- src/tests/data/backend-company.sql
+++ src/tests/data/backend-company.sql
@@ -2,3 +2,6 @@ INSERT INTO company (id, original_id, ssn, updated) VALUES ((UNHEX(REPLACE(UUID(
 INSERT INTO company (id, original_id, ssn, updated) VALUES ((UNHEX(REPLACE(UUID(), "-",""))), '124','010170-999R', NOW());
 INSERT INTO company (id, original_id, ssn, updated) VALUES ((UNHEX(REPLACE(UUID(), "-",""))), '125','010170-999R', NOW());
 INSERT INTO company (id, original_id, ssn, updated) VALUES ((UNHEX(REPLACE(UUID(), "-",""))), '126','12345567', NOW());
+INSERT INTO company (id, original_id, ssn, updated) VALUES ((UNHEX(REPLACE(UUID(), "-",""))), '222','010200A9618', NOW());
+INSERT INTO company (id, original_id, ssn, updated) VALUES ((UNHEX(REPLACE(UUID(), "-",""))), '333','290296-7808', NOW());
+
